ff7f7906c85c99fd50bbfc420514f851470819cd
abdurion <33905314+abdurion@users.noreply.github.com>
2021-09-17 20:41:09 +0300

    fixes

---
+++ dist/_nuxt/static/1631900436/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{articles:[{slug:"ماهو الأمن السيبراني؟",description:"ماهو الأمن السيبراني وماهية الأساسيات المتبعة لتطبيقة؟ وماذا نعني بـ CIA",title:"ماهو الأمن السيبراني؟ وماهي الأساسيات المتبعة عند تطبيقة؟",img:"\u002Fimg\u002Fcia_thumbnail.png",updatedAt:"2021-09-17T17:25:07.430Z"}]}],fetch:{},mutations:void 0});
