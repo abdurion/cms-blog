88389f513fe638a7410f89e5138fc5f10f9c034e
abdurion <33905314+abdurion@users.noreply.github.com>
2021-09-17 19:30:01 +0300

    Delete plugins directory

--- plugins/helpers.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-Vue.mixin({
-  methods: {
-    formatDate(date) {
-      const options = { year: "numeric", month: "long", day: "numeric" };
-      return new Date(date).toLocaleDateString("en", options);
-    }
-  }
-});
